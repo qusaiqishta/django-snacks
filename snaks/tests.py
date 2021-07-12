de40e27b7891de743d31c028a60e2bc1a461acca
qusaiqishta <qeshtaqusai0@gmail.com>
2021-07-12 12:42:23 +0300

    add tests0

--- snaks/tests.py
+++ snaks/tests.py
@@ -1,3 +1,5 @@
+from unittest.case import expectedFailure
+from django import urls
 from django.test import TestCase , SimpleTestCase
 from django.urls import reverse
 
@@ -8,6 +10,33 @@ class SnakesTests(SimpleTestCase):
         actual_status_code = self.client.get(url).status_code
         expected_status_code = 200
         self.assertEqual(actual_status_code, expected_status_code)
+
+    def test_about_page_status_code(self):
+        url=reverse('about')    
+        actual_status_code=self.client.get(url).status_code
+        expected_status_code=200
+        self.assertEqual(actual_status_code,expected_status_code)
+
+    def test_template_use_home(self):
+        url=reverse('home')    
+        actual_template_name=self.client.get(url).templates[0].name
+        expected_template_name='home.html'
+        self.assertEqual(actual_template_name,expected_template_name)
+
+    def test_template_use_about(self):
+        url=reverse('about')    
+        actual_template_name=self.client.get(url).templates[0].name
+        expected_template_name='about.html'
+        self.assertEqual(actual_template_name,expected_template_name)
+
+    def test_template_use_home(self):
+        url=reverse('about')    
+        actual_template_name=self.client.get(url).templates[1].name
+        expected_template_name='base.html'
+        self.assertEqual(actual_template_name,expected_template_name)
+    
+    
+
 
 
 # Create your tests here.
